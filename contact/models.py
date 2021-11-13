81a33b64074a9dd667a5992f0c272591dc63e0d9
MSProgrammerPy <None>
2021-11-13 22:41:11 +0330

    max_length field 'phone' dar model 'ContactUs' az 11 be 20 change shod. Chon max length in field toye ContactForm ham 20 e.

--- contact/models.py
+++ contact/models.py
@@ -28,7 +28,7 @@ class SocialNetwork(models.Model):
 class ContactUs(models.Model):
     name = models.CharField(max_length=50, verbose_name="نام بنگاه")
     consultant_name = models.CharField(max_length=20, verbose_name="نام مشاور")
-    phone = models.CharField(max_length=11, verbose_name="شماره تماس")
+    phone = models.CharField(max_length=20, verbose_name="شماره تماس")
     text = models.TextField(verbose_name="آدرس بنگاه و توضیحات بیشتر")
     date = models.DateTimeField(auto_now_add=True, null=True, verbose_name="تاریخ")
     is_read = models.BooleanField(default=False, verbose_name="خوانده شده")
